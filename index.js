7670e46307cba18155b0e732c695a3ce895efc09
Tanmay Mandwekar <136910963+TanmayMandwekar@users.noreply.github.com>
2024-08-23 10:40:31 +0530

    Portfolio 2024

---
+++ index.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const box = document.querySelector('.blinking');
+    setTimeout(() => {
+        box.classList.add('noBorder');
+    }, 3500); // 4000 milliseconds = 4 seconds
+});
+
+// Cursor
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove' , (e) => {
+    cursor.style.left = e.clientX + 'px' ;
+    cursor.style.top = e.clientY + 'px' ; 
+})
+
+
+// Page transition
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    function pageTransition() {
+        let t1 = gsap.timeline();
+    
+        t1.to(".transition", {
+            duration: 2 ,
+            scaleY: 1 ,
+            transformOrigin : "bottom" ,
+            ease: "power4.inOut",
+        }) ;
+    
+        t1.to(".transition", {
+            duration: 3 ,
+            scaleY: 0 ,
+            transformOrigin : "top" ,
+            ease: "power4.inOut",
+            delay: 3,
+        }) ;
+    }
+    
+    function delay(n) {
+        n = n || 0 ;
+        return new Promise((done) =>{
+            setTimeout(() => {
+                done();
+            }, n);
+        });
+    }  
+        barba.init({
+            sync: true,
+            transitions: [
+                {
+                    async leave(data) {
+                        const done = this.async();
+                        pageTransition();
+                        await delay(3000);
+                        done();
+                    }
+                }
+            ]
+        })
+        
+        const box = document.querySelector('.blinking');
+    setTimeout(() => {
+        box.classList.add('noBorder');
+    }, 3500); // 4000 milliseconds = 4 seconds
+    
+
+})
